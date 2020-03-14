83edf9b973d7b37c0aa15526561950ce4c2ac3db
Erik <erikmd@mail.ru>
2020-03-15 02:46:35 +0400

    Init commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_CONNECTION_URI,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+
+
